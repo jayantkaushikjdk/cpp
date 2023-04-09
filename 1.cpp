ba46b4a1147c0adb08e083941f92c86d6eb7361a
Jayant Kaushik <jayantkaushikjdk@gmail.com>
2023-04-09 11:06:57 +0530

    2

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,37 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//
+// int r=1;
+// int n;
+// cin>>n;
+// int a=64;
+// int x=1;
+// while(r<=n){
+// int c=1;
+// while(c<=r){
+//
+// cout<<char(n-c+1)<<" ";
+// int s=x++;
+// c++;
+// }cout<<endl;
+// r++;}
+// return 0;
+// }
+#include <iostream>
 using namespace std;
-int main(){
-    int a=8;
-    int b=6;
-    cout<<(a==b) <<endl;
-    cout<<a+5;
-} 
+int main()
+{
+    int i, j, n;
+    cin >> n;
+    for (i = 1; i <= n;)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            cout <<j;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int s,a[s];
+    cout<<"Enter size";
+    cin>>s;
+    for(int i=0;i<=s;i++){
+        cin>>a[i];
+
+    }
+     for(int i=0;i<=s;i++){
+     cout<<a[i]<<endl;
+ }
+return 0;
+}
