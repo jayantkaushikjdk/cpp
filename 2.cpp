946ad8d253e82ad0de2b13f90242d5f39cb1fd1f
Jayant Kaushik <jayantkaushikjdk@gmail.com>
2023-04-09 11:22:55 +0530

    array

--- 2.cpp
+++ 2.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int s,a[s];
-    cout<<"Enter size";
+    int s,a[100];
+    cout<<"Enter size ";
     cin>>s;
-    for(int i=0;i<=s;i++){
+    for(int i=0;i<s;i++){
         cin>>a[i];
 
     }
-     for(int i=0;i<=s;i++){
-     cout<<a[i]<<endl;
+     for(int i=0;i<s;i++){
+     cout<<a[i]<<" "; 
  }
 return 0;
 }
